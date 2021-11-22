bcf88cc212c2fd5512fe991e8c284bab2b45f179
Abdullah Najam <36923260+abdullahnajam@users.noreply.github.com>
2021-11-22 10:08:25 +0500

    backup

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:propertymarket/admin/admin_search_list.dart';
 import 'package:propertymarket/language_selection.dart';
 import 'package:propertymarket/navigator/bottom_navigation.dart';
+import 'package:propertymarket/screens/message.dart';
 import 'package:propertymarket/values/constants.dart';
 import 'package:propertymarket/values/shared_prefs.dart';
 import 'package:video_player/video_player.dart';
@@ -31,8 +33,23 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
     _controller = VideoPlayerController.asset("assets/video/splash.mp4");
     _initializeVideoPlayerFuture = _controller.initialize();
-    _loadWidget();
-    getCurrentUser();
+    FirebaseFirestore.instance.collection('data').doc('data').get().then((DocumentSnapshot documentSnapshot) {
+      if (documentSnapshot.exists) {
+        Map<String, dynamic> data = documentSnapshot.data() as Map<String, dynamic>;
+        if(data['active']=='yes'){
+          _loadWidget();
+          getCurrentUser();
+        }
+        else{
+          Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => Message(data['message'])));
+        }
+
+      }
+      else {
+        print('Document does not exist on the database');
+      }
+    });
+
   }
   _loadWidget() async {
     var _duration = Duration(seconds: splashDelay);
